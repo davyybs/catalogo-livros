6313bcedc47bca87b94d2439bc663e9a253aeca5
davyybs <daviferreira828@gmail.com>
2026-06-19 20:57:12 -0300

    feat: create search page

---
+++ src/app/pages/search/search.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonBackButton, IonButtons } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.page.html',
+  styleUrls: ['./search.page.scss'],
+  standalone: true,
+  imports: [IonButtons, IonBackButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+})
+export class SearchPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
