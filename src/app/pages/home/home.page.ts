a53581fe889ea4565bcb00f4495d5b6018281139
davyybs <daviferreira828@gmail.com>
2026-06-20 23:20:15 -0300

    feat: add books in home page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonIcon, IonLabel, IonItem } from '@ionic/angular/standalone';
+import { Component, OnInit, inject } from '@angular/core';
+import { IonContent, IonHeader, IonToolbar, IonButton, IonIcon, IonItem, IonCard, IonCardTitle, IonCardHeader, IonCardSubtitle, IonCardContent, IonImg } from '@ionic/angular/standalone';
 import { RouterLink } from '@angular/router';
+import { FirebaseService } from 'src/app/services/firebase.service';
 import { addIcons } from 'ionicons';
 import { search, book } from 'ionicons/icons';
 
@@ -9,15 +10,30 @@ import { search, book } from 'ionicons/icons';
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonItem, IonLabel, RouterLink, IonIcon, IonButton, IonContent, IonHeader, IonTitle, IonToolbar]
+  imports: [IonImg, IonCardContent, IonCardSubtitle, IonCardHeader, IonCardTitle, IonCard, IonItem, RouterLink, IonIcon, IonButton, IonContent, IonHeader, IonToolbar]
 })
 export class HomePage implements OnInit {
+  private firebaseService = inject(FirebaseService);
+
+  books: any[] = [];
 
   constructor() {
     addIcons({book,search});
   }
 
   ngOnInit() {
+    this.loadBookshelf();
+  }
+
+  loadBookshelf() {
+    this.firebaseService.obterEstante().subscribe({
+      next: (firebaseData) => {
+        this.books = firebaseData;
+      },
+      error: (err) => {
+        console.error("Erro ao buscar estante", err)
+      }
+    })
   }
 
 }
