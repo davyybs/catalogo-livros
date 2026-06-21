eaeb65f3d907d3f6441f08708057c5ddb8c5d016
davyybs <daviferreira828@gmail.com>
2026-06-20 22:12:10 -0300

    feat: add environment example

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  apiKey: '',
+
+  firebaseConfig: {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+  }
+};
