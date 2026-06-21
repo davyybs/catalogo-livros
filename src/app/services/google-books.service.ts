d01b4c4bc46ff9a3fa0dd2f1d47e7667f2ab327d
davyybs <daviferreira828@gmail.com>
2026-06-21 18:06:44 -0300

    feat: add getBookById

--- src/app/services/google-books.service.ts
+++ src/app/services/google-books.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Firestore ,doc, getDoc } from '@angular/fire/firestore';
 import { Observable, map } from 'rxjs';
 import { environment } from './../../environments/environment';
 
@@ -10,10 +11,17 @@ export class GoogleBooksService {
   private http = inject(HttpClient);
   private apiUrl = 'https://www.googleapis.com/books/v1/volumes';
   private apiKey = environment.apiKey;
+  private db = inject(Firestore);
 
   getBooks(query: string): Observable<any[]> {
     return this.http.get<any>(`${this.apiUrl}?q=${encodeURIComponent(query)}&key=${this.apiKey}`).pipe(
       map(response => response.items || [])
     );
+  }
+
+  async getBookById(id: string) {
+    const bookDoc = doc(this.db, 'meus_livros', id);
+    const snapshot = await getDoc(bookDoc);
+    return snapshot.exists() ? {id: snapshot.id, ...snapshot.data() } : null;
   }
 }
