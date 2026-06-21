4504e9604a03a8187db0c65ff765b4d2d21d4bc2
davyybs <daviferreira828@gmail.com>
2026-06-21 18:08:29 -0300

    feat: add books info, edit and remove functions

--- src/app/pages/book/book.page.ts
+++ src/app/pages/book/book.page.ts
@@ -1,20 +1,70 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonToolbar, IonItem, IonIcon, IonButton, AlertController, IonInput, IonLabel, IonSelect, IonSelectOption, IonBackButton } from '@ionic/angular/standalone';
+import { RouterLink, Router, ActivatedRoute } from '@angular/router';
+import { GoogleBooksService } from 'src/app/services/google-books.service';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { addIcons } from 'ionicons';
+import { book,search, trash, save } from 'ionicons/icons';
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.page.html',
   styleUrls: ['./book.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonBackButton, IonSelect, IonSelectOption, IonLabel, IonInput, IonContent, IonButton, IonIcon, IonItem, IonHeader, IonToolbar, RouterLink, FormsModule]
 })
 export class BookPage implements OnInit {
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+  private googleBooksService = inject(GoogleBooksService);
+  private firebaseService = inject(FirebaseService);
+  private alertController = inject(AlertController)
 
-  constructor() { }
+  bookId: string = '';
+  book: any = null;
 
-  ngOnInit() {
+  constructor() {
+    addIcons({search,save,trash,book});
   }
 
+  async ngOnInit() {
+    this.bookId = this.route.snapshot.paramMap.get('id') || '';
+
+    if (this.bookId) {
+      this.book = await this.googleBooksService.getBookById(this.bookId);
+    }
+  }
+
+  async saveChanges() {
+    try {
+      await this.firebaseService.atualizarProgresso(this.bookId, {
+        status: this.book.status,
+        paginasLidas: Number(this.book.paginasLidas)
+      });
+      this.router.navigate(['/home']);
+    } catch (err) {
+      console.error('Erro ao atualizar:', err);
+    }
+  }
+
+  async confirmarExclusao() {
+    const alert = await this.alertController.create({
+      header: 'Remover Livro',
+      message: 'Tem certeza que deseja remover este livro da sua estante?',
+      buttons: [
+        { text: 'Cancelar', role: 'cancel' },
+        {
+          text: 'Remover',
+          role: 'destructive',
+          handler: async () => {
+            await this.firebaseService.removerLivro(this.bookId);
+            this.router.navigate(['/home']);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
